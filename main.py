36dd472ad880282a8ab1b39686f1a7ec395de923
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-10-24 16:51:33 +0300

    gitignore

---
+++ main.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, CommandHandler
+
+
+updater = Updater(
+    token='1156998329:AAHGaJj867D9qdfQixKrhEWqdepmshWS0_k'
+    )
+dispatcher = updater.dispatcher
+
+def start(upd, ctx):
+    ctx.bot.send_message(
+        chat_id=upd.effective_chat.id,
+        text="Hello World!",
+    )
+
+def jokes(upd, ctx):
+    ctx.bot.send_message(
+        chat_id=upd.effective_chat.id,
+        text="Анекдот: в детдоме сиротам раздали сок Моя семья!",
+    )
+
+start_handler = CommandHandler(
+    'start', start
+    )
+
+jokes_handler = CommandHandler(
+    'jokes', jokes
+    )
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(jokes_handler)
+
+updater.start_polling()
